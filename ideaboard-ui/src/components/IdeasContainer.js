9697df238c7859e19dd69232bb54d0646e3fe85e
Bhavik Patel <Bhavikpatel576@gmail.com>
2017-10-09 19:23:47 -0700

    add notification when user inputs an idea

--- ideaboard-ui/src/components/IdeasContainer.js
+++ ideaboard-ui/src/components/IdeasContainer.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import Idea from './Idea'
+import IdeaForm from './IdeaForm'
+import update from "immutability-helper"
 
 class IdeasContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      ideas: []
+      ideas: [],
+      editingIdeaId: null,
+      notification: ''
     }
   }
   
@@ -20,10 +24,22 @@ class IdeasContainer extends Component {
 
   addNewItem = () => {
     axios.post("http://localhost:3001/api/v1/ideas", {idea: {title: '', body: ''}})
-      .then(response => {console.log(response)
+      .then(response => {
+        console.log(response)
+        //insert the new idea, splice to obtain the info
+        const ideas = update(this.state.ideas, { $splice: [[0,0, response.data]]})
+        this.setState({ideas: ideas, editingIdeaId: response.data.id})
       })
       .catch(error => console.log(error))
   }
+
+  updateIdea = (idea) => {
+    const ideaIndex = this.state.idea.findIndex(x => x.id === idea.id)
+    const ideas = update(this.state.ideas, {[ideaIndex]: {$set: idea }})
+    this.setState({ideas: ideas, notification: 'All changes saved'})
+  }
+
+  resetNotification = () => {this.setState({notification: ''})}
 
   render() {
     return (
@@ -32,9 +48,17 @@ class IdeasContainer extends Component {
           <button className="NewButtonIdea" onClick={this.addNewItem}>
             New Idea
           </button> 
+          <span className='notification'>
+            {this.state.notication}
+          </span>
         </div>
           {this.state.ideas.map((idea) => {
-            return (<Idea idea={idea} key={idea.id} />)
+            if(this.state.editingIdeaId === idea.id){
+              return(<IdeaForm idea={idea} key={idea.id} updateIdea={this.updateIdea} resetNotification={this.resetNotification}/>)
+            } else{
+              return (<Idea idea={idea} key={idea.id} />)  
+            }
+            
           })}
       </div>
     );
